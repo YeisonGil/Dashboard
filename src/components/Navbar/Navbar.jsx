891e1177e2b1bf4d9cfdfa772f49fa57481495c9
YeisonGil <yeisongil.a9701@gmail.com>
2023-09-21 15:03:57 -0500

    feat: terminado el nav y el sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,27 @@
 import "./navbar.css"
+import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { BsBell } from 'react-icons/bs'
+import { SlEnvolopeLetter } from 'react-icons/sl'
+import { BsPersonCircle } from 'react-icons/bs'
+
 
 const Navbar = () => {
   return (
-    <div className="Navbar">
-        <h2>Barra lateral</h2>
-        <ul className="Navbar__ul">
-            <li>Enlace 1</li>
-            <li>Enlace 2</li>
-            <li>Enlace 3</li>
-        </ul>
+    <div className="navbar">
+      <div className="navbar__menu">
+        <AiOutlineMenu className="navbar__menu-icon"/>
+      </div>
+      <div className="navbar__searcher">
+        <AiOutlineSearch className="navbar__searcher-icon"/>
+        <input type="text" className="navbar__searcher-input" placeholder="Search for..." />
+      </div>
+      <div className="nav2">
+        <p className="nav2__p">Have questions?<a className="nav2__p-a" href="#">contact@akveo.com</a></p>
+        <BsBell className='nav2__bell'/>
+        <SlEnvolopeLetter className='nav2__letter'/>
+        <BsPersonCircle className='nav2__person'/>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,24 @@
 import './sidebar.css'
 import { Link } from 'react-router-dom';
 import { AiFillHome } from 'react-icons/ai';
+import { AiOutlineDown } from 'react-icons/ai';
 
-
-
+ 
 function Sidebar() {
   return (
     <div className="sidebar">
       <h2 className='Titulo'><span className='Titulo__color'>Blur</span>Admin</h2>
       <ul className='sidebar__ul'>
         <li className='sidebar__li'>
-          <Link to="/" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Inicio</div></Link>
-          <Link to="/components" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Components</div></Link>
-          <Link to="/charts" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Charts</div></Link>
-          <Link to="/UiFeatures" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>UI Features</div></Link>
-          <Link to="/FormElements" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Form Elements</div></Link>
-          <Link to="/Tables" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Tables</div></Link>
-          <Link to="/Maps" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Maps</div></Link>
-          <Link to="/Pages" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Pages</div></Link>
-          <Link to="/MenuLvl1" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Menu Level 1</div></Link>
+          <Link to="/" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Inicio</div><AiOutlineDown/></Link>
+          <Link to="/components" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Components</div><AiOutlineDown/></Link>
+          <Link to="/charts" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Charts</div><AiOutlineDown/></Link>
+          <Link to="/UiFeatures" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>UI Features</div><AiOutlineDown/></Link>
+          <Link to="/FormElements" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Form Elements</div><AiOutlineDown/></Link>
+          <Link to="/Tables" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Tables</div><AiOutlineDown/></Link>
+          <Link to="/Maps" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Maps</div><AiOutlineDown/></Link>
+          <Link to="/Pages" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Pages</div><AiOutlineDown/></Link>
+          <Link to="/MenuLvl1" className='sidebar__a'><AiFillHome/><div className='sidebar__text'>Menu Level 1</div><AiOutlineDown/></Link>
         </li>
       </ul>
     </div>
